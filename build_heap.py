4967c0e62f43377083181660e8c38ad824e5d58e
ruudolfsb-codes <115581021+ruudolfsb-codes@users.noreply.github.com>
2023-03-19 23:01:04 +0200

    Add files via upload

--- build_heap.py
+++ build_heap.py
@@ -1,42 +1,46 @@
-# python3
-
-
 def build_heap(data):
     swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
-
-
+    for i in range(len(data) // 2, -1, -1):
+        sift_down(i, data, swaps)
     return swaps
 
+def sift_down(i, data, swaps):
+    min = i
+    left = 2 * i + 1
+    right = 2 * i + 2
+
+    if left < len(data) and data[left] < data[min]:
+        min = left
+    if right < len(data) and data[right] < data[min]:
+        min = right
+
+    if i != min:
+        data[i], data[min] = data[min], data[i]
+        swaps.append((i, min))
+        sift_down(min, data, swaps)
 
 def main():
+    input_type = input()
+    if "I" in input_type:
+        nave = int(input())
+        data = list(map(int, input().split()))
+    elif "F" in input_type:
+        filename = input()
+        if "a" not in filename:
+            with open("./tests/"+filename, mode='r') as fails:
+                nave = int(fails.readline())
+                data = list(map(int, fails.readline().split()))
+    else:
+        print("error")
+        return
     
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
-
-
-    # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
-
-    # checks if lenght of data is the same as the said lenght
-    assert len(data) == n
-
-    # calls function to assess the data 
-    # and give back all swaps
+    assert len(data) == nave
+    
     swaps = build_heap(data)
 
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
-
-
-    # output all swaps
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
-
 
 if __name__ == "__main__":
     main()
